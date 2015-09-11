72421e16a46825d5e49e59741247e099753fb2de
insomniac235 <juric.benjamin23@gmail.com>
2015-09-11 13:12:18 +0200

    dodo 15

---
+++ Lahko15.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lahko15;
+import java.util.Scanner;
+/**
+ *
+ * @author aa
+ */
+public class Lahko15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s=new Scanner(System.in);
+        String start=s.next();
+        String remove=s.next();
+        StringBuilder result=new StringBuilder();
+        for (int i = 0; i < start.length(); i++) {
+            
+            for (int j = 0; j < remove.length(); j++) {
+                if(start.charAt(i)==remove.charAt(j))
+                    break;
+                else if(j==remove.length()-1)
+                    result.append(start.charAt(i));
+            }
+        }
+        System.out.println(result);
+    }
+    
+}
